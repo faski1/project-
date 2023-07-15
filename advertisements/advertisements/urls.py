bd9a8ae6b8eaf36780c59517ceb69a448f6b806c
oleg <olegzykov08@mail.ru>
2023-07-15 11:20:50 +0500

    commit

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_advertisements.urls'))
+    path('', include('app_advertisements.urls')),
+    path('lesson4', include('app_lesson4.urls')),
 ]
---
+++ advertisements/app_lesson4/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import lesson4
+
+urlspatterns = [
+    path('', lesson4)
+]
