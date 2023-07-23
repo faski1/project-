74c6c249bd963d76243c429dbfd0c1352cf3bdb0
oleg <olegzykov08@mail.ru>
2023-07-23 15:06:50 +0500

    commit

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_advertisements.urls'
+    'app_advertisements'
 ]
 
 MIDDLEWARE = [
