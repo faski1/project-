3e3062ade34829e73373874e94abb59175e26457
oleg <olegzykov08@mail.ru>
2023-07-28 09:52:26 +0300

    commit

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requset):
-    return HttpResponse("Успешно!")
+def index(request):
+    return render(request, 'index.html')
