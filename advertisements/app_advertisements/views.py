8263a4df2bc4a84538d74badd8ae51f895427a4e
oleg <olegzykov08@mail.ru>
2023-08-26 01:44:24 +0500

    commit

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -26,3 +26,15 @@ def advertisement_post(request):
 
     context = {'form' : form}
     return render(request, 'advertisement-post.html', context)
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def advertisement(request):
+    return render(request, 'advertisement.html')
